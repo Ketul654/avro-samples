29a289aea01ef9f8cc211c5880c775b1c8889f53
Ketul <ptl.ketul@gmail.com>
2021-01-20 19:18:23 +0000

    schema evolution forward compatible

--- src/main/java/com/ketul/avro/evolution/ForwardCompatibleExample.java
+++ src/main/java/com/ketul/avro/evolution/ForwardCompatibleExample.java
@@ -2,6 +2,8 @@ package com.ketul.avro.evolution;
 
 import com.ketul.avro.schema.EmployeeV1;
 import com.ketul.avro.schema.EmployeeV2;
+import com.ketul.avro.schema.EmployeeV3;
+import com.ketul.avro.schema.EmployeeV4;
 import org.apache.avro.file.DataFileReader;
 import org.apache.avro.file.DataFileWriter;
 import org.apache.avro.io.DatumReader;
@@ -10,43 +12,44 @@ import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificDatumWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BackwardCompatibleExample {
+public class ForwardCompatibleExample {
     private static final Logger LOGGER = LoggerFactory.getLogger(BackwardCompatibleExample.class);
 
     public static void main(String[] args) {
 
         // Create employee record
-        EmployeeV1 employeeV1 = getEmployee();
+        EmployeeV4 employeeV4 = getEmployee();
 
-        LOGGER.info(employeeV1.toString());
+        LOGGER.info(employeeV4.toString());
 
         // Write to file
-        File file = new File("target//employee-v1.avro");
-        boolean isWritten = writeToFile(file, employeeV1);
+        File file = new File("target//employee-v4.avro");
+        boolean isWritten = writeToFile(file, employeeV4);
 
         if(isWritten) {
             // Read from file
-            List<EmployeeV2> employeeRecords = readFromFile(file);
+            List<EmployeeV3> employeeRecords = readFromFile(file);
             employeeRecords.forEach((employeeRecord -> LOGGER.info(employeeRecord.toString())));
         }
     }
 
-    private static List<EmployeeV2> readFromFile(File file) {
-        LOGGER.info("Reading from file {} using schema V2", file.getAbsolutePath());
+    private static List<EmployeeV3> readFromFile(File file) {
+        LOGGER.info("Reading from file {} using schema V3", file.getAbsolutePath());
         /*
          * Explicitly mention which schema you are going to use to read
-         * EmployeeV2.class indicates its employeeV2.avsc
+         * EmployeeV3.class indicates its employeeV3.avsc
          */
-        DatumReader<EmployeeV2> datumReader = new SpecificDatumReader<>(EmployeeV2.class);
-        List<EmployeeV2> employees = new ArrayList<>();
-        try(DataFileReader<EmployeeV2> employeeReader = new DataFileReader<>(file, datumReader)) {
+        DatumReader<EmployeeV3> datumReader = new SpecificDatumReader<>(EmployeeV3.class);
+        List<EmployeeV3> employees = new ArrayList<>();
+        try(DataFileReader<EmployeeV3> employeeReader = new DataFileReader<>(file, datumReader)) {
             while (employeeReader.hasNext()) {
-                EmployeeV2 employeeRecord = employeeReader.next();
+                EmployeeV3 employeeRecord = employeeReader.next();
                 employees.add(employeeRecord);
             }
         } catch (IOException e) {
@@ -55,12 +58,12 @@ public class BackwardCompatibleExample {
         return employees;
     }
 
-    private static boolean writeToFile(File file, EmployeeV1 employeeV1) {
-        LOGGER.info("Writing from file {} using schema V1", file.getAbsolutePath());
-        DatumWriter<EmployeeV1> datumWriter = new SpecificDatumWriter<>();
-        try(DataFileWriter<EmployeeV1> employeeDataWriter = new DataFileWriter<>(datumWriter)) {
-            employeeDataWriter.create(employeeV1.getSchema(), file);
-            employeeDataWriter.append(employeeV1);
+    private static boolean writeToFile(File file, EmployeeV4 employeeV4) {
+        LOGGER.info("Writing from file {} using schema V4", file.getAbsolutePath());
+        DatumWriter<EmployeeV4> datumWriter = new SpecificDatumWriter<>();
+        try(DataFileWriter<EmployeeV4> employeeDataWriter = new DataFileWriter<>(datumWriter)) {
+            employeeDataWriter.create(employeeV4.getSchema(), file);
+            employeeDataWriter.append(employeeV4);
             LOGGER.info("Written to {}", file.getAbsolutePath());
             return true;
         } catch (IOException e) {
@@ -69,14 +72,15 @@ public class BackwardCompatibleExample {
         }
     }
 
-    private static EmployeeV1 getEmployee(){
-        EmployeeV1.Builder employeeBuilder = EmployeeV1.newBuilder();
+    private static EmployeeV4 getEmployee(){
+        EmployeeV4.Builder employeeBuilder = EmployeeV4.newBuilder();
         employeeBuilder.setEmployeeId("BK128746XT");
         employeeBuilder.setFistName("Shiva");
         employeeBuilder.setLastName("Kailasha");
         employeeBuilder.setAge(20);
         employeeBuilder.setSalary(100000.34f);
         employeeBuilder.setPhoneNumber("444-5555-66666");
+        employeeBuilder.setEmailId("abc@xyz.com");
         return employeeBuilder.build();
     }
 }
